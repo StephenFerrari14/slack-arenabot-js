39c6da60bcaf5dedaad42061fca3250c9d28a843
Stephen Ferrari <stephen.ferrari14@gmail.com>
2019-03-03 11:05:19 -0500

    Small adjustments to the data and tried to make scheduler might need redis

--- scripts/arena.js
+++ scripts/arena.js
@@ -3,9 +3,9 @@
 //
 // Commands:
 //   hubot create robot <name> - Create a new bot
-//   hubot delete robot - Delete your current bot
-//   hubot get bot - Get information on your current bot
-//   hubot fight <user> - Challenge a user to a fight
+//   hubot delete my robot - Delete your current bot
+//   hubot show my robot - Get information on your current bot
+//   hubot challenge <user> - Challenge a user to a fight
 //
 // Configuration:
 //   HUBOT_HELP_REPLY_IN_PRIVATE - if set to any value, all `hubot help` replies are sent in private
@@ -24,24 +24,36 @@ module.exports = (robot) => {
   const switchboard = new Conversation(robot);
 
   robot.respond(/create robot (.*)/i, (res) => {
-    const user = res.message.user.name;
+    const user = res.message.user.id;
     const name = res.match[1];
     // Add dialog for overriding right now
-    try {
-      newRobot = factory.createRobot(user, name)
-      factory.saveRobot(robot, newRobot);
-      res.reply('Created Robot :robot_face:\nStats: ' + JSON.stringify(newRobot));
-    } catch(error) {
-      res.reply('Failed to create robot. Please try again.')
-    }
-  });
-
-  robot.hear(/delete robot/i, (res) => {
-    res.reply('Are you sure?');
+    res.reply('Creating a new robot overrides your current one. Do you still want to do it?');
     const dialog = switchboard.startDialog(res);
     dialog.addChoice(/yes|Yes/, (res2) => {
       try {
-        factory.deleteRobot(robot, res2.message.user.name);
+        newRobot = factory.createRobot(user, name)
+        factory.saveRobot(robot, newRobot);
+        const statsMessage = 
+        '>*Stats:*\n' +
+        '>*Name :* ' + newRobot.name + '\n' +
+        '>*HP      :* ' + newRobot.hp + '\n' +
+        '>*Attack:* ' + newRobot.attack;
+        res2.reply('Created Robot :robot_face:\n' + statsMessage);
+      } catch(error) {
+        res2.reply('Failed to create robot. Please try again.')
+      }
+    });
+    dialog.addChoice(/no|No/, function(res2){ res2.reply('Thanks.') });
+    dialog.addChoice(/.*/, function(res2){ res2.reply('Please ask again and answer with "yes" or "no".') });
+  });
+
+  robot.respond(/delete my robot/i, (res) => {
+    res.reply('Are you sure?');
+    const user = res.message.user.id;
+    const dialog = switchboard.startDialog(res);
+    dialog.addChoice(/yes|Yes/, (res2) => {
+      try {
+        factory.deleteRobot(robot, user);
         res2.reply('Robot deleted :robot_face:');
       } catch (error) {
         res2.reply('There was trouble deleting your robot, try again.');
@@ -52,50 +64,65 @@ module.exports = (robot) => {
     // The dialog will expire after 30 seconds
   });
 
-  robot.hear(/get robot/i, (res) => {
-    const user = res.message.user.name;
-    console.log(res.messsage.user);
+  robot.respond(/get my robot|show my robot/i, (res) => {
+    const user = res.message.user.id;
+    // console.log(res.message.user);
     const robert = factory.loadRobot(robot, user);
     if (robert) {
-      res.reply(':robot_face: Robot assembled...\n' + JSON.stringify(robert));
+      const statsMessage =
+      '>*Stats:*\n' +
+      '>*Name :* ' + robert.name + '\n' +
+      '>*HP      :* ' + robert.hp + '\n' +
+      '>*Attack:* ' + robert.attack + '\n' +
+      '>*XP      :* ' + robert.xp;
+      res.reply(':robot_face: Robot assembled...\n' + statsMessage);
     } else {
       res.reply('You do not have a robot, Please make one.');
     }
   });
 
-  robot.hear(/fight (.+)/i, (res) => {
-    const id1 = res.message.user.name;
-    const id2 = res.match[1];
+  robot.respond(/challenge (.+)/i, (res) => {
+    const challenger = res.message.user.id;
+    let id2 = res.match[1]; // To use id here we need to get it from the user but that might be easier when they confirm
+    id2 = id2.replace('@', '');
 
-    if (id1 === id2) {
+    // console.log(robot.brain.userForName('testbot'));
+
+    const challengee = robot.brain.userForName(id2)
+
+    if (challenger === challengee) {
       res.reply('You can\'t challenge yourself.');
       return;
     }
-    const robert  = factory.loadRobot(robot, id1);
-    const robert2 = factory.loadRobot(robot, id2);
+    const robert  = factory.loadRobot(robot, challenger);
+    const robert2 = factory.loadRobot(robot, challengee.id);
     if (!robert) {
       res.reply('Your robot doesn\'t exist, please create one :robot_face:');
       return;
     }
     if (!robert2) {
-      res.reply('Enemy doesn\'t exist.');
+      res.reply('Enemy doesn\'t have a robot.');
       return;
     }
+    // See if the user accepts a challenge?
+    // const dialog = new Conversation(robot, 'user');
+    // // const challengeeMessage = robot.messageRoom(challengee, 'Would you like to challenge @' + challengee + ' in a Slack Bot Battle!');
+    // console.log(challengeeMessage);
     try {
       const winner = factory.startFight(robert, robert2);
       if (factory.gainXp(robot, winner, 1)) {
-        res.reply('Winner is ' + winner.name + ' :robot_face:!');
+        res.send('Winner is ' + winner.name + ' :robot_face:!');
         return;
       }
       throw "Error saving experience for " + winner.name + " of " + winner.owner;
     } catch(error) {
       console.log(error); // Replace console logs with logger
-      res.reply('Technical difficulties during the fight, repair your robots and try again.')
+      res.send('Technical difficulties during the fight, repair your robots and try again.')
     }
   });
 
   robot.hear(/set test robots/i, (res) => {
-    robot.brain.set('testbot', {'name': 'Testbot', 'owner': 'testbot', 'hp': 1, 'attack': 1, 'xp': 1});
+    robot.brain.set('U84UA7KGS', {'name': 'Testbot', 'owner': 'U84UA7KGS', 'hp': 1, 'attack': 1, 'xp': 1});
     robot.brain.set('the ultimate robot', {'name': 'The Ultimate Robot', 'owner': 'the ultimate robot', 'hp': 1000000, 'attack': 1000000, 'xp': 0});
-  })
+  });
 }
